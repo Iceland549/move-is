67092a7e13b29733728cb9c12d3b50f8ae23511d
chaud <chaud@MOA>
2024-12-15 08:39:23 +0100

    Ajoutez des fichiers projet.

---
+++ Move-Is/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Move_Is.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Move-Is/Models/Movie.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace Move_Is.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Range(1900, 2100)]
+        public string Country { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Synopsis { get; set; }
+
+        [Range(0, 10)]
+        public double Rating { get; set; }
+
+        public string ImageUrl { get; set; }
+        
+    }
+}
